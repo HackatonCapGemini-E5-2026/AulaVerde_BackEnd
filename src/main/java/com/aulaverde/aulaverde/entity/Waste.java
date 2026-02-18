d1d0a6821eb6fde1ba9bd4b50ef62f02a7775b1f
Sukaina <sukisu91@gmail.com>
2026-02-18 23:45:34 +0100

    feat:dev branch first commit

--- src/main/java/com/aulaverde/aulaverde/entity/Waste.java
+++ src/main/java/com/aulaverde/aulaverde/entity/Waste.java
@@ -1,5 +1,29 @@
 package com.aulaverde.aulaverde.entity;
 
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "residuos")
+@Data
+@NoArgsConstructor
 public class Waste {
+@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Double peso;
+
+
+    @Column(nullable = false)
+    private LocalDate fecha;
 
 }
