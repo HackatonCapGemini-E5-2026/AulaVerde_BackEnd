652f34c5f3b693d745e1f8e49102e7ddef4a6b76
Sukaina <sukisu91@gmail.com>
2026-02-18 22:14:31 +0100

    first commit

---
+++ src/main/java/com/aulaverde/aulaverde/AulaverdeApplication.java
@@ -0,0 +1,13 @@
+package com.aulaverde.aulaverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaverdeApplication.class, args);
+	}
+
+}
