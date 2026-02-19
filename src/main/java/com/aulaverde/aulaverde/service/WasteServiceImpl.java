8eb6565174f954a46537464614f20cd6b2921362
ManonChab <godfroymanon@gmail.com>
2026-02-19 10:19:34 +0100

    feature : Create, GetAll, GetById  and Delete done and working

--- src/main/java/com/aulaverde/aulaverde/service/WasteServiceImpl.java
+++ src/main/java/com/aulaverde/aulaverde/service/WasteServiceImpl.java
@@ -1,5 +1,44 @@
 package com.aulaverde.aulaverde.service;
 
-public class WasteServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.aulaverde.aulaverde.entity.Waste;
+import com.aulaverde.aulaverde.repository.WasteRepository;
+
+@Service
+public class WasteServiceImpl implements WasteService{
+
+    private final WasteRepository wasteRepository;
+
+    public WasteServiceImpl(WasteRepository wasteRepository){
+        this.wasteRepository=wasteRepository;
+    }
+
+    @Override
+    public Waste createWaste(Waste waste) {
+        return wasteRepository.save(waste);
+    }
+
+    @Override
+    public List<Waste> getAllWaste() {
+        return wasteRepository.findAll();
+    }
+
+    @Override
+    public Optional<Waste> getWasteById(int id) {
+        Optional<Waste> optionalWaste = wasteRepository.findById(id);
+        if(optionalWaste.isEmpty()) throw new RuntimeException("No existe articulo con este id");
+        return optionalWaste;
+    }
+
+    @Override
+    public void deleteWaste(int id) {
+        Optional<Waste> optionalWaste = wasteRepository.findById(id);
+        if(optionalWaste.isEmpty()) throw new RuntimeException("No existe articulo con este id");
+        wasteRepository.delete(optionalWaste.get());
+        }
 
 }
